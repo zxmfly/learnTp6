d6cb6b680377b0939075a340f36ffbdaaeab032a
zxmfly <zenghuayuan@126.com>
2020-03-11 18:09:40 +0800

    数据库操作

--- app/controller/DbTest.php
+++ app/controller/DbTest.php
@@ -11,4 +11,25 @@ class DbTest{
         //test
         dump($rs);
     }
+
+    public function demo2(){//execute()
+        $sql = " update `user` set age = :age WHERE  user_id = :uid ";
+        $map = ['age'=>18, 'uid'=> 1];
+        $rs = Db::execute($sql, $map);//返回成功执行的行数
+        //test
+        dump($rs);
+    }
+    /*
+     * table() 设置表
+     * field() 查下的字段
+     * find（）返回第一条记录,必须带有条件或者order 或者传入主键值 否则返回Null 你必须明确要查询什么数据
+     * */
+    public function demo3(){
+        $rs = Db::name('user')
+            ->field('*')
+            //->where('user_id','<>',1)
+            ->find(3);
+        echo  Db::name('user')->getLastSql();//输出sql
+        dump($rs);
+    }
 }
