3a70c0cf401f8f7c6cc5f5d32afdcd6ec2fa4ad6
zxmfly <zenghuayuan@126.com>
2020-04-13 23:55:01 +0800

    学习视图模板

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,12 +3,28 @@ namespace app\controller;
 
 use app\BaseController;
 use think\facade\Db;
+use think\facade\View;
 
 class Index extends BaseController
 {
     public function index()
     {
-        dump(Db::query("select * from `user` limit 1"));
+        // 模板变量赋值
+        //View::assign('name','ThinkPHP');
+        //View::assign('email','thinkphp@qq.com');
+        // 或者批量赋值
+        View::assign([
+            'name'  => 'ThinkPHP',
+            'email' => 'thinkphp@qq.com'
+        ]);
+        // 模板输出
+        return View::fetch('index');
+        //return View::fetch();
+        //dump(Db::query("select * from `user` limit 1"));
+    }
+    public function user()
+    {
+        return 'user';
     }
 
     public function hello($name = 'ThinkPHP6')
